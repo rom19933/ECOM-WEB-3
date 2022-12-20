5af555662ff5a27d695b729ca4eabdd451acb599
rom19933 <rom19931993@gmail.com>
2022-12-20 20:18:35 +0300

    Firs Commit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express=require('express');
-
 const app=express();
+const morgan=require('morgan')
+const ProductControllers=require('./MVC/controllers/products');
+app.use(morgan('dev'));
+app.use(function(req,res,next)
+{
+    const myip=req.socket.controllersAddress;
+    console.log('connection form IP' + req.socket.controllersAddress);
+    next();
+});
 app.get("/",(req,res)=>{
 res.status(200).json({"Message":"hello from Ecommerce web api"});
 
 });
+app.use('/product',ProductControllers);
+
 module.exports=app;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+function sendmail(u,p);
+{
+    console.log("sent Email to " + u);
+}
+sendEmail(process.env.USER_EMAIL,process.env.EMAIL_PASS);
 const http=require('http');
-
 const app=require('./app');
 const port=3030;
-
 const server=http.createServer(app,()=>{console.log("server started")});
 
 server.listen(port);
